9b830e8ef66576079caf4624b2b064de3b4e69fc
Missing Corner <48879877+haophan1507@users.noreply.github.com>
2023-02-05 10:05:06 +0700

    set up library

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ThemeProvider } from './ThemeProvider';
+import { BrowserRouter } from 'react-router-dom';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </BrowserRouter>
+    </ThemeProvider>
+  </React.StrictMode>
+);
